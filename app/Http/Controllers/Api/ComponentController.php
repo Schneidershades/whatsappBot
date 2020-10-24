b87aa80f65aec7c3490a1a5481e038c5d78022ab
Schneider Shades <schneidershades@gmail.com>
2020-10-25 00:26:59 +0100

    push

--- app/Http/Controllers/Api/ComponentController.php
+++ app/Http/Controllers/Api/ComponentController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Component;
 
 class ComponentController extends Controller
 {
     public function index()
     {
-    	return Brand::all();
+    	return Component::all();
     }
 }
