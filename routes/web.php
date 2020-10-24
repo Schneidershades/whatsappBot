2f436de8acc2c919a423614ed92f10e4491ded0a
Schneider Shades <schneidershades@gmail.com>
2020-10-24 22:45:58 +0100

    push

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tables', function () {
+     return $tables = DB::select('SHOW TABLES'); // returns an array of stdObjects
+});
