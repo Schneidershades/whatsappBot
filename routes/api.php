0fd60f1a86528b9776c19ef3c011ffb624301a4d
Schneider Shades <schneidershades@gmail.com>
2020-10-25 00:50:00 +0100

    push

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/chat-bot', 'Chat\ChatBotController@listenToReplies');
 Route::resource('/brands', 'Api\BrandController');
 Route::resource('/components', 'Api\ComponentController');
+Route::resource('/category', 'Api\CategoryController');
+Route::resource('/models', 'Api\ModelController');
+Route::resource('/makes', 'Api\MakeController');
+Route::resource('/year', 'Api\YearController');
