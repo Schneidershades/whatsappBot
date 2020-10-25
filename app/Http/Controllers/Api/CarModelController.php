678a7261b45a0c8a5fc24e7c4cd78183bd60b9ae
Schneider Shades <schneidershades@gmail.com>
2020-10-25 01:18:05 +0100

    car

--- app/Http/Controllers/Api/CarModelController.php
+++ app/Http/Controllers/Api/CarModelController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Model;
+use App\Models\CarModel;
 
-class ModelController extends Controller
+class CarModelController extends Controller
 {
     public function index()
     {
