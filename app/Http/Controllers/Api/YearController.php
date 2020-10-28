1e574eb186cbb37f275ce5cb673eb34672d802f5
Schneider Shades <schneidershades@gmail.com>
2020-10-28 20:56:25 +0100

    bu

--- app/Http/Controllers/Api/YearController.php
+++ app/Http/Controllers/Api/YearController.php
@@ -17,8 +17,10 @@ class YearController extends Controller
 
         $bodyItems = explode(" ", strtolower($body));
 
-
         return $this->allTables($bodyItems);
+
+
+        
 
   //       if(in_array('find', $bodyItems) && in_array('vehicle', $bodyItems)){
 		// 	return 'ggo';
@@ -92,10 +94,10 @@ class YearController extends Controller
 
     public function allTables($items)
     {
-    	$year = Year::where('year', $year)->distinct()->get()->pluck('year')->toArray();
-    	$make = Make::where('company', $year)->distinct()->get()->pluck('company')->toArray();
-    	$model = Model::where('model', $year)->distinct()->get()->pluck('model')->toArray();
-    	$component = Component::where('component', $year)->distinct()->get()->pluck('component')->toArray();
+    	$year = Year::whereIn('year', $items)->distinct()->get()->pluck('year')->toArray();
+    	$make = Make::whereIn('company', $items)->distinct()->get()->pluck('company')->toArray();
+    	$model = Model::whereIn('model', $items)->distinct()->get()->pluck('model')->toArray();
+    	$component = Component::whereIn('component', $items)->distinct()->get()->pluck('component')->toArray();
 
     	$collection = array_merge($year, $make, $model, $component);
 
