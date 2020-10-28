6388f0c3dacbb9677b086eebd6a3211fc016a027
Schneider Shades <schneidershades@gmail.com>
2020-10-28 01:30:01 +0100

    push

--- app/Http/Controllers/Api/YearController.php
+++ app/Http/Controllers/Api/YearController.php
@@ -10,9 +10,17 @@ class YearController extends Controller
 {
     public function index()
     {
-    	return Year::select('year')
+    	$years = Year::select('year')
 			->distinct()
 			->get();
+
+        $replies = [];
+
+		foreach ($years as $year) {
+            array_push($replies, $year['year']);
+		}
+
+		return $replies;
     }
 
     public function show($year)
