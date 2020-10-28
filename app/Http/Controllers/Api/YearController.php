88a3cc0d86910e2699f669457d0704fedf0124f4
Schneider Shades <schneidershades@gmail.com>
2020-10-28 21:34:57 +0100

    !=null

--- app/Http/Controllers/Api/YearController.php
+++ app/Http/Controllers/Api/YearController.php
@@ -114,27 +114,32 @@ class YearController extends Controller
 
     public function confirmTable($item)
     {
+    	$year = null;
+    	$make = null;
+    	$model = null;
+    	$component = null;
+    	
     	$year = Year::where('year', $item)->distinct()->get()->pluck('year')->toArray();
 
-    	if($year){
+    	if($year!=null){
     		return ['year' => $year->year];
     	}
 
     	$make = Make::where('company', $item)->distinct()->get()->pluck('company')->toArray();
 
-    	if($make){
+    	if($make!=null){
     		return ['make' => $make->company];
     	}
 
     	$model = CarModel::where('model', $item)->distinct()->get()->pluck('model')->toArray();
 
-    	if($model){
+    	if($model!=null){
     		return ['model' => $model->model];
     	}
 
     	$component = Component::where('component', $item)->distinct()->get()->pluck('component')->toArray();
 
-    	if($component){
+    	if($component!=null){
     		return ['component' => $component->component];
     	}
 
