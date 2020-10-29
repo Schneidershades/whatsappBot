dae22f3233c2c20fe129280b36b248b2006b332e
Schneider Shades <schneidershades@gmail.com>
2020-10-29 23:26:38 +0100

    push

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -8,6 +8,7 @@ use App\Models\Year;
 use App\Models\Make;
 use App\Models\CarModel;
 use App\Models\Component;
+use App\Models\BotSearchRequest;
 
 class SearchController extends Controller
 {
@@ -27,12 +28,67 @@ class SearchController extends Controller
 
 	    	$message = null;
 
+	    	if($replies == null || $replies == []){
+	            $newChat = new BotSearchRequest;
+	            $newChat->phone = $from;
+	            $newChat->request_received = $body;
+	            $newChat->save();
+	        }
+
+
 	    	foreach($years as $year){
 	    		$message .= $year ." \n ";
 	    	}
 
 	    	return $message;
+
+	    	// return $this->sendWhatsAppMessage($message, $from);
 		}
+
+		if(in_array('2000', $bodyItems)){
+
+	    	$message = null;
+
+			$phone = BotSearchRequest::where('phone', $from)
+				->where('terminate', 'no')
+				->where('finished', 'no')
+				->first();
+
+			if(!$phone){
+				return 'ss';
+			}
+
+			if($phone->stage_model == 'year'){
+				$yearItems = Year::where('year', $year)->get()->pluck('makeid')->toArray();
+				$makeids = Make::whereIn('makeid', $yearItems)->get()->pluck('company')->toArray();
+
+				foreach($makeids as $make){
+		    		$message .= $make ." \n ";
+		    	}
+
+		    	return $message;
+			}
+
+			if($phone->stage_model == 'make'){
+				
+			}
+
+			if($phone->stage_model == 'component'){
+				
+			}
+
+			$phone->save();
+
+
+	    	
+	    	// return $this->sendWhatsAppMessage($message, $from);
+		}
+
+
+
+
+
+
 
 
     }
@@ -43,8 +99,9 @@ class SearchController extends Controller
 	    		->distinct()
 	    		->orderBy('year')
 	    		->get()
-	    		->pluck('year')
-	    		->toArray();
+	    		// ->pluck('year')
+	    		// ->toArray()
+	    		;
 	    return $year;
     }
 }
