d86fd49c16b0271958738e802241c23a79b4a960
Schneider Shades <schneidershades@gmail.com>
2020-10-30 05:20:52 +0100

    cr

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -78,6 +78,7 @@ class SearchController extends Controller
 					return $message;
 				}
 
+				dd($phone);
 
 				if($phone->stage_model == 'make' &&  $phone->make == null){
 
@@ -102,7 +103,7 @@ class SearchController extends Controller
 			    		$message .= $model->modelid . " - " . $makeId->make.' - '. $model->model . " \n ";
 			    	}
 
-
+			    	
 					return $message;
 
 			    	// $phone->stage_model = 'make';
