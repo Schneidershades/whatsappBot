ee3efc75bc3fb82bfb61b609ad1aa70498ca3d96
Schneider Shades <schneidershades@gmail.com>
2020-10-30 01:08:19 +0100

    ce

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -36,8 +36,6 @@ class SearchController extends Controller
 			    	$phone->finished = true;
 			    	$phone->save();
 	        	}
-
-	        	dd($phone);
 	        	
 				if($phone->stage_model == 'new' && $phone->year == null){
 					return $this->newStage($body);
