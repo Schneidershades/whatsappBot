cf1a01a7786e7a33a7bec404dd1ee31e166c4167
Schneider Shades <schneidershades@gmail.com>
2020-11-06 06:58:02 +0100

    ni

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -25,14 +25,13 @@ class SearchController extends Controller
         $phone = $this->dbSavedRequest($from, $body);
 
         if($phone->stage_model == 'new'){
-            return $phone;
-            // if($body == "F2"){
-            //     // $phone->stage_model = 'yearShortList';
-            //     $phone->save();
-            //     // return $phone->stage_model;
-            //     // $message .= $this->yearShortList($from, $body);
-            //     dd($body, $phone->stage_model);
-            // }
+            if($body == "F2"){
+                // $phone->stage_model = 'yearShortList';
+                // $phone->save();
+                // return $phone->stage_model;
+                // $message .= $this->yearShortList($from, $body);
+                dd($body, $phone->stage_model);
+            }
 
         }
 
