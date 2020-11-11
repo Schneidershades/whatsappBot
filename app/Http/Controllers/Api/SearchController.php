e0dbc33076f10efba83ed4319bb71b2a34edf149
Schneider Shades <schneidershades@gmail.com>
2020-11-11 10:46:50 +0100

    xw

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -370,7 +370,7 @@ class SearchController extends Controller
         if($items){
             $models = CarModel::whereIn('makeid', $items)->get();
 
-            dd($items, $models, $phone->year, $phone->make_id);
+            // dd($items, $models, $phone->year, $phone->make_id);
 
             foreach($models as $model){
                 $message .= $model->modelid . " - " . $model->model . " \n ";
