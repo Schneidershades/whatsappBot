5cb90d83f359be79a4d8afc92f64238b34137d77
Schneider Shades <schneidershades@gmail.com>
2020-11-11 05:26:10 +0100

    ni

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/chat-bot', 'Chat\ChatBotController@listenToReplies');
+Route::resource('/chat-bot', 'Chat\ChatBotController');
 Route::resource('/brands', 'Api\BrandController');
 Route::resource('/components', 'Api\ComponentController');
 Route::resource('/category', 'Api\CategoryController');
