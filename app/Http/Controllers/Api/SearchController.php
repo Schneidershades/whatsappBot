38e29b46e0b004ccf8494d17a7cd2965142b4f7c
Schneider Shades <schneidershades@gmail.com>
2020-11-11 07:08:47 +0100

    xw

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -332,10 +332,10 @@ class SearchController extends Controller
             return $this->yearShortList($from, $body);
         }
 
-        dd($body);
-
         $yearItems = Year::where('year', $phone->year)
               ->where('makeid', $body)->first();
+
+        dd($yearItems);
 
         if($yearItems){ 
 
