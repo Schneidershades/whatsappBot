ffe0dc2f802bf92747bddbdd1c1900324d2590cd
Schneider Shades <schneidershades@gmail.com>
2020-11-11 06:00:52 +0100

    ecce

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -336,6 +336,8 @@ class SearchController extends Controller
         $yearItems = Year::where('year', $phone->year)
               ->where('makeid', $body)->first();
 
+        dd($yearItems);
+
         if($yearItems){ 
 
             $make = Make::where('makeid', $yearItems->makeid)->first();
